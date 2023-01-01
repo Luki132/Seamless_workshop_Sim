de2f889ee430bd0abc50e40a95248676fdf6182d
ufqox <manthan.talekar@student.kit.edu>
2023-01-01 20:59:12 +0100

    Turtlebot Manual Navigation

---
+++ turtlebot_controller.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import math
+import rospy
+import tf
+from geometry_msgs.msg import Twist, Point, Pose, Quaternion
+from nav_msgs.msg import Odometry
+
+first_condition = False
+second_condition = False
+third_condition = False
+fourth_condition = False
+fifth_condition = False
+sixth_condition = False
+seventh_condition = False
+eighth_condition = False
+ninth_condition = False
+tenth_condition = False
+eleventh_condition = False
+twelfth_condition = False
+thirteenth_condition = False
+fourteenth_condition = False
+fifteenth_condition = False
+
+def euler_from_quaternion(x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+     
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+     
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+     
+    return roll_x, pitch_y, yaw_z # in radians
+
+def odom_callback(odom: Odometry):
+    global first_condition
+    global second_condition
+    global third_condition
+    global fourth_condition
+    global fifth_condition
+    global sixth_condition
+    global seventh_condition
+    global eighth_condition
+    global ninth_condition
+    global tenth_condition
+    global eleventh_condition
+    global twelfth_condition
+    global thirteenth_condition
+    global fourteenth_condition
+    global fifteenth_condition
+
+    cmd = Twist()
+    x_rad, y_rad, z_rad = euler_from_quaternion(odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w)
+    z_degree = math.degrees(z_rad)
+    print(z_degree)
+    
+    if odom.pose.pose.position.x > 1.15 and odom.pose.pose.position.x < 1.3 and first_condition == False:
+        rospy.loginfo("START.")
+        cmd.linear.x = 0.22
+        cmd.angular.z = 0.0
+
+    elif odom.pose.pose.position.x > 1.0 and odom.pose.pose.position.x < 1.16 and z_degree < -90 and second_condition == False:
+        rospy.loginfo("First condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.3
+        first_condition = True
+
+    elif z_degree > -90 and third_condition == False:
+        rospy.loginfo("Second condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
+        second_condition = True
+        third_condition = True
+        rospy.sleep(1)
+
+    elif odom.pose.pose.position.y > 0.5 and odom.pose.pose.position.y < 0.8 and z_degree > -95 and fourth_condition == False:
+        rospy.loginfo("Third condition achieved.")
+        cmd.linear.x = 0.1
+        cmd.angular.z = 0.0
+
+    elif odom.pose.pose.position.y > 0.4 and odom.pose.pose.position.y < 0.51 and z_degree < 0 and z_degree > -95 and fifth_condition == False:
+        rospy.loginfo("Fourth condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.3
+        fourth_condition = True
+
+    elif z_degree > 0 and sixth_condition == False:
+        rospy.loginfo("Fifth condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
+        fifth_condition = True
+        sixth_condition = True
+        rospy.sleep(1)
+
+    elif z_degree > -10 and odom.pose.pose.position.x > 0.5 and seventh_condition == False:
+        rospy.loginfo("Sixth condition achieved.")
+        cmd.linear.x = -0.2
+        cmd.angular.z = 0.0
+        
+    elif odom.pose.pose.position.x < 0.5 and seventh_condition == False and eighth_condition == False:
+        rospy.loginfo("Seventh condition achieved. Waiting 10s.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
+        rospy.sleep(10)
+        seventh_condition = True
+
+    elif odom.pose.pose.position.x < 1.15 and seventh_condition == True and ninth_condition == False:
+        rospy.loginfo("Eighth condition achieved.")
+        cmd.linear.x = 0.2
+        cmd.angular.z = 0.0
+        eighth_condition = True
+
+    elif odom.pose.pose.position.x > 1.15 and eighth_condition == True and ninth_condition == False and tenth_condition == False:
+        rospy.loginfo("Ninth condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
+        ninth_condition = True
+
+    elif z_degree > -90 and ninth_condition == True and eleventh_condition == False:
+        rospy.loginfo("Tenth condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = -0.3
+        tenth_condition = True
+
+    elif z_degree < -90 and tenth_condition == True and twelfth_condition == False:
+        rospy.loginfo("Eleventh condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
+        eleventh_condition = True
+        twelfth_condition = True
+        rospy.sleep(1)
+
+    elif odom.pose.pose.position.y < 0.78 and z_degree > -100 and twelfth_condition == True and fourteenth_condition == False:
+        rospy.loginfo("Twelfth condition achieved.")
+        cmd.linear.x = -0.05
+        cmd.angular.z = 0.0
+        thirteenth_condition = True
+
+    elif odom.pose.pose.position.y > 0.75 and z_degree > -179 and thirteenth_condition == True and fifteenth_condition == False:
+        rospy.loginfo("Thirteenth condition achieved.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = -0.3
+        fourteenth_condition = True
+
+    elif z_degree < -179 and fourteenth_condition == True:
+        rospy.loginfo("STOP.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
+        fifteenth_condition = True
+        rospy.sleep(1)
+
+    else:
+        rospy.loginfo("OUT OF DEFINED COORDINATES.")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
+        
+
+    pub.publish(cmd)
+
+
+if __name__ == '__main__':
+    rospy.init_node("turtlebot_controller")
+    pub = rospy.Publisher("/turtlebot1/cmd_vel", Twist, queue_size=10)
+    sub = rospy.Subscriber("/turtlebot1/odom", Odometry,
+                           callback=odom_callback)
+    rospy.loginfo("Node has been started.")
+    rate = rospy.Rate(50)
+    rospy.spin()
+
