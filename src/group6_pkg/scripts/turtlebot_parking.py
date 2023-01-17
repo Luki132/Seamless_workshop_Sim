0f7ebd7363d3c0d671fa2b85fff6f62745c6e87e
Caleb <uiwub@student.kit.edu>
2023-01-17 15:50:38 +0100

    Updated Parking

--- src/group6_pkg/scripts/turtlebot_parking.py
+++ src/group6_pkg/scripts/turtlebot_parking.py
@@ -9,11 +9,12 @@ import math
 # max angular speed is 2.84rad/sec
 angular_z = 0.1
 
-def stop():
-    cmd = Twist()
-    rospy.loginfo("STOP.")
-    cmd.linear.x = 0.0
-    cmd.angular.z = 0.0  
+# def stop():
+#     global cmd
+#     cmd = Twist()
+#     rospy.loginfo("STOP.")
+#     cmd.linear.x = 0.0
+#     cmd.angular.z = 0.0  
 
 def euler_from_quaternion(x, y, z, w):
     t0 = +2.0 * (w * x + y * z)
@@ -69,46 +70,48 @@ def charuco_detector_callback(park: PoseStamped):
 
     if park.pose.position.x > 0.01 and park.pose.position.x < 0.04 and park.pose.position.z > 0.15:
         rospy.loginfo("happy path towards marker.")
-        cmd.linear.x = -0.15
+        cmd.linear.x = -0.05
         cmd.angular.z = 0.0
     elif park.pose.position.z < 0.15:
-        cmd.linear.x = -0.15
+        cmd.linear.x = -0.0005
         angular_z = angular_z*(-1)
         cmd.angular.z = angular_z
 
-    if park.pose.position.z < 0.085:
-       stop()
+    if park.pose.position.z < 0.1:
+        print("BINGO")
+        cmd.linear.x = 0.0
+        cmd.angular.z = 0.0
             
     # print(park.pose.position.z)  
     pub.publish(cmd)
 
-def odom_callback(data: Odometry):
-    global cmd
+# def odom_callback(data: Odometry):
+#     global cmd
 
-    cmd = Twist()
+#     cmd = Twist()
 
-    x_rad, y_rad, z_rad = euler_from_quaternion(data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
-    z_degree = math.degrees(z_rad)
-    # z_sign = math.degrees(z_rad)
+#     x_rad, y_rad, z_rad = euler_from_quaternion(data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
+#     z_degree = math.degrees(z_rad)
+#     # z_sign = math.degrees(z_rad)
 
-    # print(z_degree)
-    # print(data.pose.pose.position.x)
+#     # print(z_degree)
+#     # print(data.pose.pose.position.x)
 
-    if z_degree < -90 and z_degree > -180 and data.pose.pose.position.x < 1.15:
-        if z_degree < -90 and z_degree > -175:
-            cmd.linear.x = 0.0
-            cmd.angular.z = -0.3
-        else:
-            rospy.loginfo("First condition achieved.")
-            cmd.linear.x = -0.08
-            cmd.angular.z = 0.0
-    # if data.pose.pose.position.x < 1.15 and z_degree < 180 and z_degree > 175:
-    #     rospy.loginfo("First condition achieved.")
-    #     cmd.linear.x = -0.08
-    #     cmd.angular.z = 0.0
-    # elif z_degree < 0 and z_degree > 0:
+#     if z_degree < -90 and z_degree > -180 and data.pose.pose.position.x < 1.15:
+#         if z_degree < -90 and z_degree > -175:
+#             cmd.linear.x = 0.0
+#             cmd.angular.z = -0.3
+#         else:
+#             rospy.loginfo("First condition achieved.")
+#             cmd.linear.x = -0.08
+#             cmd.angular.z = 0.0
+#     # if data.pose.pose.position.x < 1.15 and z_degree < 180 and z_degree > 175:
+#     #     rospy.loginfo("First condition achieved.")
+#     #     cmd.linear.x = -0.08
+#     #     cmd.angular.z = 0.0
+#     # elif z_degree < 0 and z_degree > 0:
 
-    pub.publish(cmd)
+#     pub.publish(cmd)
 
 # def odom_callback(data: Odometry):
 #     global cmd
@@ -130,7 +133,7 @@ def odom_callback(data: Odometry):
 if __name__ == '__main__':
     rospy.init_node("turtlebot_controller")
     pub = rospy.Publisher("/turtlebot1/cmd_vel", Twist, queue_size=10)
-    sub = rospy.Subscriber("/turtlebot1/odom", Odometry,callback=odom_callback)
+    # sub = rospy.Subscriber("/turtlebot1/odom", Odometry,callback=odom_callback)
     sub1 = rospy.Subscriber("/turtlebot1/camera/image_charuco_pose", PoseStamped,callback=charuco_detector_callback)
     rospy.loginfo("Node has been started.")
 
