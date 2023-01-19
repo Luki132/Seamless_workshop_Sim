5ec785fb95c36ef724eab1e9c408bb43b2afbaf1
Max Zuidberg <max.zuidberg@student.kit.edu>
2023-01-19 12:15:42 +0100

    nothing to see here

---
+++ src/group6_pkg/src/parking_clean.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import rospy
+import tf
+from geometry_msgs.msg import Twist, Point, Pose, Quaternion, PoseStamped
+from nav_msgs.msg import Odometry
+import math
+import time
+from dataclasses import dataclass
+
+
+
+class Condition:
+    def __init__(self, **kwargs):
+        
+
+@dataclass
+class Pose:
+
+
+@dataclass
+class Point:
+
+
+
+points = [
+    {
+        "condition": {
+            ""
+        }
+    }
+]
