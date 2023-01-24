ea0ec87bb32390434e5d3973d6e44cafcf13f18d
Caleb <uiwub@student.kit.edu>
2023-01-24 22:47:25 +0100

    Updated

--- src/group6_pkg/scripts/online_cube_detectio.py
+++ src/group6_pkg/scripts/online_cube_detectio.py
@@ -2,8 +2,11 @@ from cv_bridge import CvBridge
 import rospy
 import cv2
 import numpy as np
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image, CompressedImage, CameraInfo
 import math    
+from image_geometry import PinholeCameraModel
+from geometry_msgs.msg import Pose, PoseArray, Point
+
 bridge = CvBridge()
 
 counter = 0
@@ -37,6 +40,14 @@ tray_pos = np.array([[0], [0]])
 second_method = False
 test_green = np.empty((1,2))
 angle = 0.0
+
+z_diff = 0.6104 - 0.158
+
+x_to_world = [None, None, None]
+y_to_world = [None, None, None]
+x_to_world_float = [None, None, None]
+y_to_world_float = [None, None, None]
+
 
 # def find_orientation(arr1: np.array, arr2: np.array):
 #     """ arr1: tray, arr2: lidar"""
@@ -100,7 +111,10 @@ def circle_detector(img):
             # print("found")
 
 
-def callback(data):    
+def callback(data):   
+    global camera
+    coordinates = PoseArray()
+ 
     global tray_found, circle_found,test_green, tray_pos, circle_pos, cv_image, edges, all_mask, out, second_method,counter, angle
 
 
@@ -108,35 +122,33 @@ def callback(data):
 
     overall_candidate = np.zeros((9,6))
 
-    if second_method == False:
-        second_method = True
-        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
         # cv_image = cv2.imread('/home/robis/image_23012023/cv_img_'+ str(counter) + '.jpg')
         # cv_image = cv2.imread('/home/robis/cv_image4.jpg')
 
-        if counter < 280:
-            counter = counter + 1
-        else:
-            counter = 0
-        hsv_frame = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        grayFrame = cv2.normalize(cv_image,None, 150, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    if counter < 280:
+        counter = counter + 1
+    else:
+        counter = 0
+    hsv_frame = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+    grayFrame = cv2.normalize(cv_image,None, 150, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
 
-        grayFrame = cv2.cvtColor(grayFrame, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(cv_image, threshold1= 100, threshold2=255)
+    grayFrame = cv2.cvtColor(grayFrame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(cv_image, threshold1= 100, threshold2=255)
 
-        white_mask = cv2.inRange(hsv_frame, white_lower, white_upper)
-        black_mask = cv2.inRange(hsv_frame, black_lower, black_upper)
-        blue_mask = cv2.inRange(hsv_frame, blue_lower, blue_upper)
-        red_mask1 = cv2.inRange(hsv_frame, red_lower1, red_upper1)
-        red_mask2 = cv2.inRange(hsv_frame, red_lower2, red_upper2)
-        green_mask = cv2.inRange(hsv_frame, green_lower, green_upper)
-        all_mask = white_mask + blue_mask + red_mask1 + red_mask2 + green_mask
+    white_mask = cv2.inRange(hsv_frame, white_lower, white_upper)
+    black_mask = cv2.inRange(hsv_frame, black_lower, black_upper)
+    blue_mask = cv2.inRange(hsv_frame, blue_lower, blue_upper)
+    red_mask1 = cv2.inRange(hsv_frame, red_lower1, red_upper1)
+    red_mask2 = cv2.inRange(hsv_frame, red_lower2, red_upper2)
+    green_mask = cv2.inRange(hsv_frame, green_lower, green_upper)
+    all_mask = white_mask + blue_mask + red_mask1 + red_mask2 + green_mask
 
-        ret, out = cv2.threshold(all_mask, 190, 255, cv2.THRESH_BINARY)
-        out = cv2.dilate(out, kernel)
+    ret, out = cv2.threshold(all_mask, 190, 255, cv2.THRESH_BINARY)
+    out = cv2.dilate(out, kernel)
 
-        edges = cv2.Canny(grayFrame, threshold1= 0, threshold2=255)
-        # edges = cv2.dilate(edges, kernel)
+    edges = cv2.Canny(cv_image, threshold1= 100, threshold2=255)
+    edges = cv2.dilate(edges, kernel)
 
         # black_mask = cv2.bitwise_not(black_mask)
         # black_mask = cv2.bitwise_or(black_mask, edges)
@@ -185,117 +197,6 @@ def callback(data):
         #         circle_found = True
         #         print("found")
 
-        circle_detector(grayFrame)
-
-        contours, hierarchy = cv2.findContours(out, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(cv_image, contours,-1,(0,0,255),1)
-
-        for count, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if(area > 4000 and area < 15000):
-                x, y, w, h = cv2.boundingRect(contour)
-                if x < 740 and x > 0 and w < 170 and h < 170:
-                # if x > 0:
-                    info_green = np.array([[x, y, w, h, area, 2]])
-                    rect = cv2.minAreaRect(contour)
-                    angle = rect[-1]
-                    print("Rotation:", angle)
-                    box = cv2.boxPoints(rect)
-
-                    rectangle = np.int0(box)
-                    u_pos = int((rectangle[0][0] + rectangle[1][0] + rectangle[2][0] + rectangle[3][0])/4)
-                    v_pos = int((rectangle[0][1] + rectangle[1][1] + rectangle[2][1] + rectangle[3][1])/4)
-                    cv2.circle(cv_image, (u_pos, v_pos), 4, 2)
-                    cv2.drawContours(cv_image,[rectangle],0,(0,0,0),2)
-                    test_green = np.append(test_green, box, axis = 0)
-                    tray_found = True
-                    tray_pos = np.array([[u_pos],[v_pos]])
-                    # print("found")
-
-        test_green = np.delete(test_green, 0, 0)
-        if tray_found == True and circle_found == True:
-            # print(tray_pos)
-            # print(circle_pos)
-            second_method = False
-        else:
-            second_method = True
-
-    else:
-        second_method = False
-        circle_found = False
-        tray_found = False
-        print("Back-up Plan")
-        test_green = np.empty((1,2))
-
-        overall_candidate = np.zeros((9,6))
-        # cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
-        # cv_image = cv2.imread('/home/robis/cv_image8.jpg')
-        hsv_frame = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        grayFrame = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-
-
-        edges = cv2.Canny(grayFrame, threshold1= 0, threshold2=255)
-        edges = cv2.dilate(edges, kernel)
-        # edges = cv2.dilate(edges, kernel)
-        # edges = cv2.dilate(edges, kernel)
-
-        circle_detector(grayFrame)
-
-        ret, thresh = cv2.threshold(grayFrame, 180, 255, cv2.THRESH_BINARY)
-        white_mask = cv2.inRange(hsv_frame, white_lower, white_upper)
-        blue_mask = cv2.inRange(hsv_frame, blue_lower, blue_upper)
-        red_mask1 = cv2.inRange(hsv_frame, red_lower1, red_upper1)
-        red_mask2 = cv2.inRange(hsv_frame, red_lower2, red_upper2)
-        green_mask = cv2.inRange(hsv_frame, green_lower, green_upper)
-        all_mask = cv2.bitwise_or(white_mask, red_mask1)
-        all_mask = cv2.bitwise_or(all_mask, red_mask2)
-        all_mask = cv2.bitwise_or(all_mask, blue_mask)
-        all_mask = cv2.bitwise_or(all_mask, green_mask)
-
-
-            
-        # white_mask + blue_mask + red_mask1 + red_mask2 + green_mask
-        # all_mask = cv2.bitwise_or(thresh, all_mask)
-        out2 = cv2.bitwise_not(all_mask)
-        out2 = cv2.bitwise_or(out2, edges)
-        out = cv2.bitwise_not(out2)
-        out = cv2.dilate(out, kernel)
-        # out = cv2.morphologyEx(out, cv2.MORPH_CLOSE, kernel)
-
-        # ret, out = cv2.threshold(out, 190, 255, cv2.THRESH_BINARY)
-
-        contours, hierarchy = cv2.findContours(out, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(cv_image, contours,-1,(0,0,255),1)
-
-        for count, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            # print(count, area)
-            if(area > 4000 and area < 15000):
-            # if area > 0:
-                # print("Bingo", area)
-                x, y, w, h = cv2.boundingRect(contour)
-                if x < 740 and x > 0 and w < 130 and h < 130:
-                    info_green = np.array([[x, y, w, h, area, 2]])
-                    rect = cv2.minAreaRect(contour)
-                    box = cv2.boxPoints(rect)
-                    angle = rect[-1]
-                    rectangle = np.int0(box)
-                    u_pos = int((rectangle[0][0] + rectangle[1][0] + rectangle[2][0] + rectangle[3][0])/4)
-                    v_pos = int((rectangle[0][1] + rectangle[1][1] + rectangle[2][1] + rectangle[3][1])/4)
-                    cv2.circle(cv_image, (u_pos, v_pos), 4, 2)
-
-                    cv2.drawContours(cv_image,[rectangle],0,(0,0,0),2)
-                    test_green = np.append(test_green, box, axis = 0)
-                    tray_found = True
-                    tray_pos = np.array([[u_pos],[v_pos]])
-
-        test_green = np.delete(test_green, 0, 0)
-        if tray_found == True and circle_found == True:
-            # print(tray_pos)
-            # print(circle_pos)
-            second_method = True
-        else:
-            second_method = False
 
     hsv_mask = cv2.bitwise_or(red_mask1, red_mask2)
     hsv_mask = cv2.bitwise_or(hsv_mask, red_mask2)
@@ -308,13 +209,14 @@ def callback(data):
 
     contours, hierarchy = cv2.findContours(hsv_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-
+    counter = 0
     for count, contour in enumerate(contours):
         area = cv2.contourArea(contour)
         if(area > 1000 and area < 5000):
             x, y, w, h = cv2.boundingRect(contour)
-            if x < 1280/2 and x > 150 and w < 170 and h < 170:
+            if x < 850 and x > 150 and w < 170 and h < 170:
                 # if x > 0:
+        
                 info_green = np.array([[x, y, w, h, area, 2]])
                 rect = cv2.minAreaRect(contour)
                 box = cv2.boxPoints(rect)
@@ -322,17 +224,27 @@ def callback(data):
                 rectangle = np.int0(box)
                 u_pos = int((rectangle[0][0] + rectangle[1][0] + rectangle[2][0] + rectangle[3][0])/4)
                 v_pos = int((rectangle[0][1] + rectangle[1][1] + rectangle[2][1] + rectangle[3][1])/4)
+                y_old, x_old, z_old = camera.projectPixelTo3dRay((u_pos,v_pos)) ## Keep in mind that the pixel here is flip
+                x_to_world_float[counter] = round(z_diff/z_old*x_old + 0.54, 3)
+                y_to_world_float[counter] = round(z_diff/z_old*y_old + 0.49, 3)
                 cv2.circle(cv_image, (u_pos, v_pos), 4, 2)
                 cv2.drawContours(cv_image,[rectangle],0,(0,0,0),2)
                 test_green = np.append(test_green, box, axis = 0)
                 tray_found = True
                 tray_pos = np.array([[u_pos],[v_pos]])
-                print("found")
+                print(counter)
+                counter = counter + 1
+    # print(test_green)
 
-    if circle_pos[0] != 0 and tray_pos[0] != 0 and  tray_found == True and circle_found == True:
-        turtlebot_ang = round(find_orientation(tray_pos, circle_pos, angle),2)
-        # print(angle)
-        cv2.putText(cv_image, "TurtleBot Orientation is: " + str(angle), (10, 650), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0))
+    coord1 = Pose(position=Point(x=x_to_world_float[0], y=y_to_world_float[0], z=0.158))
+    coord2 = Pose(position=Point(x=x_to_world_float[1], y=y_to_world_float[1], z=0.158))
+    coord3 = Pose(position=Point(x=x_to_world_float[2], y=y_to_world_float[2], z=0.158))
+
+    coordinates.poses.append(coord1)
+    coordinates.poses.append(coord2)
+    coordinates.poses.append(coord3)
+
+    coordinate_publisher.publish(coordinates)
 
     tray_found = False
     circle_found = False
@@ -341,7 +253,7 @@ def callback(data):
     cv2.imshow('Kinect Camera', cv_image)
     # cv2.imshow('Binary', out2)
     # cv2.imshow('Black mask', black_mask)
-    # cv2.imshow('Canny', edges)
+    cv2.imshow('Canny', edges)
     cv2.imshow('HSV', hsv_mask)
     cv2.imshow('Output', out)
 
@@ -355,11 +267,26 @@ def callback(data):
     cv2.waitKey(1)
 
 def listener():
+    global camera
+
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/kinect/rgb_camera/image_raw", Image, callback)
+    rospy.Subscriber("/rgb/camera_info", Image, callback)
+    coordinate_publisher = rospy.Publisher("/cargo_position", PoseArray, queue_size=10 )
+
+    camera_info_msg = rospy.wait_for_message("/kinect1/rgb_camera/camera_info", CameraInfo)
+    camera = PinholeCameraModel()
+    camera.fromCameraInfo(camera_info_msg)
     rospy.spin()
 
 if __name__=='__main__':
     # while(True):
     #     callback()
-    listener()
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/kinect/rgb_camera/image_raw", Image, callback)
+    coordinate_publisher = rospy.Publisher("/cargo_position", PoseArray, queue_size=10 )
+
+    camera_info_msg = rospy.wait_for_message("/rgb/camera_info", CameraInfo)
+    camera = PinholeCameraModel()
+    camera.fromCameraInfo(camera_info_msg)
+    rospy.spin()
+    # listener()
